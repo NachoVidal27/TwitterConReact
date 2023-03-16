06e74e7bb188ab0814fe61e665bd3dcdb60999d5
NachoVidal27 <igjovidal@gmail.com>
2023-03-16 16:24:19 -0300

    comit facu

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -4,20 +4,22 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function Tweets() {
-  const token = useState((state) => state.user.token);
+  const token = useSelector((state) => state.user.token);
   console.log(token);
   const [tweets, setTweets] = useState();
+
   useEffect(() => {
-    console.log("milanesa");
+    console.log("estamos ejecutando useEffect");
     const getTweets = async () => {
       const response = await axios({
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTA5MWUyZmQ1YTUzOGY3YWM3MTM2MCIsImlhdCI6MTY3ODk5MzM5M30.zhf74cr8VTDcWASET8x7HQLHw2f1JOKpFN2gEzcrVaw`,
         },
         method: "get",
         url: "http://localhost:8000/tweets",
       });
       setTweets(response.data);
+      console.log("la response del axios es " + response);
     };
     getTweets();
   }, []);
